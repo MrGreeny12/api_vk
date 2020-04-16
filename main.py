9a9f87850cd6b553192516e847573976b2298421
Ivakhnenko Viktor <44995425+MrGreeny12@users.noreply.github.com>
2020-04-16 21:29:47 +0300

    edit main

--- main.py
+++ main.py
@@ -1,52 +1,123 @@
+from pprint import pprint
+from urllib.parse import urlencode
 
-import request from requests
-import pprint
+import requests
 
-APP_ID = 7298778
-AUTH_URL = 'https://oauth.vk.com/authorize'
-params = {
-	'client_id' : APP_ID,
-	'display' : 'page',
-	'scope' : '',
-	'responce_type' : 'token',
-	'v' : 5.103
-}
+def auth():
+    APP_ID = 7382587
+    AUTH_URL = 'https://oauth.vk.com/authorize'
+    oparams = {
+        'client_id': APP_ID,
+        'display': 'page',
+        'scope': 'friends',
+        'response_type': 'token',
+        'v': 5.52
+    }
+    print('?'.join((AUTH_URL, urlencode(oparams))))
 
-TOKEN = '17b9af7e4d876c6b82d33196845d88b2b6664aecc69e7546c46d356be0bffd83d3d410502fe1dc66f2e91'
-user_id = 192091799 #внести айди того, кого хочешь посмотреть
+TOKEN = '198bade3d62e3d604091a7fdc7a48844055d697d3b3aa41c2973d0261a27678319cdfbee5c2e4e9293ccd'
 
 class User:
-	def __init__(self, token, user_id):
-		self.token = token
-		self.user_id = user_id
+    '''
+    Класс принимает на входе: token, id пользователя
+    '''
+    def __init__(self, token, id_vk):
+        self.token = token
+        self.id_vk = id_vk
 
-	def get_params(self):
-		return {
-			'user_id' : self.user_id,
-			'order' : 'name',
-			'list_id' : '',
-			'count' : 1000,
-			'offset' : '5',
-			'fields' : '',
-			'name_case' : 'ins',
-			'ref' : '',
-			'access_token' : TOKEN,
-			'v': 5.103
-		}
+    def get_params(self):
+        return {
+            'access_token': self.token,
+            'user_id': self.id_vk,
+            'order': 'hints',
+            'count': '200',
+            # 'fields': 'first_name, last_name',
+            'name_case': 'nom',
+            'v': 5.52
+        }
 
-	def get_friends_list(self, kwargs=None):
-		params = self.get_params
-		responce = request.get(
-			'https://api.vk.com/method/friends.get',
-			params
-		)
+    def friends_get(self):
+        '''
+        принимают параметры запроса
+        :return: словарь с данными по методу friends.get vk
+        '''
+        params = self.get_params()
+        responce = requests.get(
+            'https://api.vk.com/method/friends.get',
+            params
+        )
+        return responce.json()
 
-		return responce.json()
+    def common_friends(self, other_user):
+        '''
+        производит сравнение списков id двух экземпляров класса
+        :param other_user: экземпляр класса, с которым нужно сравнить
+        :return: список id общих друзей
+        '''
+        self.other_user_fr_list = other_user.friends_get()['response']['items']
+        self.friends_list = self.friends_get()['response']['items']
+        self.common_list = []
+        for i in self.other_user_fr_list:
+            for j in self.friends_list:
+                if i == j:
+                    self.common_list.append(i)
+                    break
+        return self.common_list
 
-	# def set_friends_list(self):
-	# 	pass
+    def user_get(self):
+        '''
+        внутренний запрос класса, получает с сервера vk данные через метод users.get
+        :return: данные с сервера в виде словаря
+        '''
+        params_2 = {
+            'access_token': self.token,
+            'user_id': self.id_vk,
+            'fields': 'domain',
+            'v': 5.52
+        }
+        params = self.get_params()
+        responce_2 = requests.get(
+            'https://api.vk.com/method/users.get',
+            params_2
+        )
+        return responce_2.json()
+
+    def profile_link(self):
+        '''
+
+        :return: генерирует ссылку на профиль в вк для экземпляра класса
+        '''
+        self.profile_domain = self.user_get()['response'][0]['domain']
+        print(f'https://vk.com/{self.profile_domain}')
+
+        # потом придумать, как из списка id - сделать список экземпляров класса
+
+# через перебор циклом, определять id - в виде нового usera
+def main():
+    '''
+    функция принимает название пользователей и возвращает список общих друзей
+    '''
+    command = input('Введите объекты поиска (например: user1 & user2, не забывая о пробелах):')
+    user_1, operator, user_2 = command.split()
+    if operator == '&':
+        search = f'{user_1}.common_friends({user_2})'
+        print(type(search))
+        pprint(search.json())
+    else:
+        print(f'Вы ввели {operator} вместо &. Повторите попытку')
 
 
-anonim = User(TOKEN, user_id)
-friends_list = anonim.get_friends_list()
-print(friends_list)
+
+# auth()
+user1 = User(TOKEN, 192091799)
+user2 = User(TOKEN, 320118571)
+print(type(user1))
+friends_list_1 = user1.friends_get()
+friends_list_2 = user2.friends_get()
+# user2.profile_link()
+# pprint(friends_list_1)
+# pprint(friends_list_2)
+common_list_1 = user1.common_friends(user2)
+# pprint(common_list_1)
+# print(user1)
+# main()
